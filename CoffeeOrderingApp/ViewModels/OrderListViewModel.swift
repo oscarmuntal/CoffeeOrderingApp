6bf6e0aa15652908f6ea3cd78e08aecf8637189d
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-07 20:48:26 +0200

    Adding the OrderViewModel and OrderListViewModel with its orders property marked as Published

---
+++ CoffeeOrderingApp/ViewModels/OrderListViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  OrderListViewModel.swift
+//  CoffeeOrderingApp
+//
+//  Created by Òscar Muntal on 7/4/23.
+//
+
+import Foundation
+
+class OrderListViewModel: ObservableObject {
+    @Published var orders = [OrderViewModel]()
+    
+    func fetchOrders() {
+        Webservice().getAllOrders { orders in
+            guard let orders = orders else { return }
+            self.orders = orders.map(OrderViewModel.init)
+        }
+    }
+}
+
+
+class OrderViewModel {
+    let id = UUID()
+    
+    var order: Order
+    
+    init(order: Order) {
+        self.order = order
+    }
+    
+    var name: String {
+        order.name
+    }
+    
+    var size: String {
+        order.size
+    }
+    
+    var coffeeName: String {
+        order.coffeeName
+    }
+    
+    var total: String {
+        order.total
+    }
+}
