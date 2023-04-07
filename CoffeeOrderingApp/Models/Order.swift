a5daf60343d81199241364ae4abb24aa8b839d56
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-07 20:20:18 +0200

    Codable model named Order

---
+++ CoffeeOrderingApp/Models/Order.swift
@@ -0,0 +1,17 @@
+//
+//  Order.swift
+//  CoffeeOrderingApp
+//
+//  Created by Òscar Muntal on 7/4/23.
+//
+
+import Foundation
+
+struct Order: Codable {
+    let name: String
+    let size: String
+    let coffeeName: String
+    let total: String
+}
+
+
